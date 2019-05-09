a3ceb94a07ce8ddcbbbb17315c4cca02578be25b
= <=>
2019-05-08 22:27:29 -0400

    adding expenses + Modal JS

--- budget/views.py
+++ budget/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project, Category
+from .models import Project, Category, Expense
 from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
 from django.utils.text import slugify
+from .forms import ExpenseForm
 
 # Create your views here.
 def project_list(request):
@@ -11,8 +12,29 @@ def project_list(request):
 def project_detail(request, project_slug): # param
     # checking existance
     project = get_object_or_404(Project, slug=project_slug)
-    expense_list = project.expenses.all()
-    return render(request,'budget/project-detail.html', {'project': project, 'expense_list': expense_list})
+
+    if request.method == 'GET':
+        expense_list = project.expenses.all()
+        category_list = Category.objects.filter(project=project)
+        return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': expense_list, 'category_list': category_list})
+
+    elif request.method == "POST":
+        # processing the form
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            amount = form.cleaned_data['amount']
+            category_name = form.cleaned_data['category']
+
+            category = get_object_or_404(Category, project=project, name=category_name)
+            Expense.objects.create(
+                project=project,
+                title=title,
+                amount=amount,
+                category=category
+            ).save()
+
+    return HttpResponseRedirect(project_slug)
 
 class ProjectCreateView(CreateView):
     model = Project
